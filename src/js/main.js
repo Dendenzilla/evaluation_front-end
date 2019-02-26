0e8407747aa18dc7ed08e2515feda9fca8e259c0
Martin Adriaensen <martinadriaensen@gmail.com>
2019-02-26 12:02:31 +0100

    fin seconde matinée

---
+++ src/js/main.js
@@ -0,0 +1,45 @@
+$('.burger').click(function(){
+    $('.menu').toggleClass('hidden');
+});
+
+$('.choice1').click(function(){
+    $('.forms__hotel, .forms__holiday, .forms__flight').removeClass('hide');
+    $('.forms__hotel, .forms__holiday').addClass('hide');
+    $('.choice1, .choice2, .choice3').removeClass('faded');
+    $('.choice2, .choice3').addClass('faded');
+    
+});
+$('.choice2').click(function(){
+    $('.forms__hotel, .forms__holiday, forms__flight').removeClass('hide');
+    $('.forms__flight, .forms__holiday').addClass('hide');
+    $('.choice1, .choice2, .choice3').removeClass('faded');
+    $('.choice1, .choice3').addClass('faded');
+    
+});
+$('.choice3').click(function(){
+    $('.forms__hotel, .forms__holiday, forms__flight').removeClass('hide');
+    $('.forms__hotel, .flight').addClass('hide');
+    $('.choice1, .choice2, .choice3').removeClass('faded');
+    $('.choice2, .choice1').addClass('faded');
+    
+});
+
+let stickyNav = $('#header').offset().top;
+
+function stickyFct() {
+    let scrollPage = $(window).scrollTop();
+    if (scrollPage > stickyNav) {
+        $('#header').addClass('sticky');
+    }else {
+        $('#header').removeClass('sticky');
+    }
+}
+$(window).scroll(function () {
+    stickyFct();
+    
+});
+$(window).resize(function () {
+    stickyNav = $('#header').offset().top;
+    stickyFct();
+});
+
